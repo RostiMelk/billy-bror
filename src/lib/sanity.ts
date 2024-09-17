078a55f72714d65c3f014fb3fb8c68c6e8055fd1
RostiMelk <hello@rosti.no>
2024-09-17 22:42:28 +0200

    feat: no cdn

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -4,13 +4,13 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "vX",
-  useCdn: true,
+  useCdn: false,
 });
 
 export const serverClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   token: process.env.NEXT_PRIVATE_SANITY_TOKEN,
-  apiVersion: "v2024-06-07",
-  useCdn: true,
+  apiVersion: "vX",
+  useCdn: false,
 });
