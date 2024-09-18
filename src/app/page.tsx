670789d58d471038d5d51570201ef6dda514305d
RostiMelk <hello@rosti.no>
2024-09-18 08:13:59 +0200

    feat: start time type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -149,7 +149,7 @@ export default function Home() {
           <AnimatePresence mode="wait">
             {activeEntry ? (
               <motion.div key="timer" {...motionProps}>
-                <Timer startTime={activeEntry.startTime} />
+                <Timer startTime={activeEntry.startTime as string} />
               </motion.div>
             ) : (
               <motion.div key="lastTrip" {...motionProps}>
