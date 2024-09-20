c69a250e0a2071da85f88119df41923f725bca70
RostiMelk <hello@rosti.no>
2024-09-20 19:34:09 +0200

    feat: use px for end time anim

--- src/components/submit-dialog.tsx
+++ src/components/submit-dialog.tsx
@@ -263,9 +263,9 @@ export const SubmitDialog = ({
                 {isOutside && (
                   <motion.div
                     key="endTime"
-                    initial={{ opacity: 0, x: "-100%" }}
+                    initial={{ opacity: 0, x: 20 }}
                     animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: "-100%" }}
+                    exit={{ opacity: 0, x: -20 }}
                   >
                     <FormField
                       control={form.control}
