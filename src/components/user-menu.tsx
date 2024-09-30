9d51fef385057f6f6ae397350332c3d288101783
RostiMelk <hello@rosti.no>
2024-09-30 21:11:47 +0200

    feat: cleanup

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useSession, signOut } from "next-auth/react";
-import { saveAs } from "file-saver";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import {
   Popover,
@@ -9,26 +8,11 @@ import {
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
-import { hashEmail } from "@/lib/utils";
-import { createSiriShortcut } from "@/lib/siri-shortcut";
 
 export const UserMenu = () => {
   const { data: session } = useSession();
 
   if (!session?.user) return null;
-
-  const handleCreateStartEntrySiriShortcut = () => {
-    if (!session?.user) {
-      throw new Error("User not authenticated");
-    }
-    const apiUrl = "https://billy.rosti.no/api/siri/start-entry";
-    const apiBody = { hash: hashEmail(session.user.email) };
-    const shortcutName = "Start trip";
-    const shortcutData = createSiriShortcut(apiUrl, apiBody, shortcutName);
-    const blob = new Blob([shortcutData], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, `${shortcutName}.shortcut`);
-    return shortcutName;
-  };
 
   return (
     <Popover>
@@ -43,15 +27,6 @@ export const UserMenu = () => {
         <p className="text-sm text-muted-foreground">{session.user.email}</p>
 
         <nav className="grid gap-2 mt-3">
-          {/*<Button
-            onClick={handleCreateStartEntrySiriShortcut}
-            size="sm"
-            className="w-full"
-            variant="ghost"
-          >
-            Lag en Siri snarvei
-          </Button>*/}
-
           <Button
             onClick={async () => await signOut()}
             size="sm"
