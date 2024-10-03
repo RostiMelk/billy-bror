25cdcfc8a95bf4b6a99ed2de7aa29d092857e5c4
RostiMelk <hello@rosti.no>
2024-10-03 15:10:22 +0200

    feat: rounded full

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -23,7 +23,7 @@ const buttonVariants = cva(
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
-        lg: "h-14 rounded-lg px-8 text-lg",
+        lg: "h-14 rounded-full px-8 text-lg",
         icon: "h-9 w-9",
       },
     },
