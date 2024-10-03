45f417854d06ec8784436c1f93fc35c1d4401625
RostiMelk <hello@rosti.no>
2024-10-03 15:15:39 +0200

    feat: adjust fade height

--- src/app/(app)/(authed)/page.tsx
+++ src/app/(app)/(authed)/page.tsx
@@ -146,7 +146,7 @@ export default function Home() {
           )}
         </Header>
 
-        <main className="overflow-auto fade grid items-center mt-1 mb-3">
+        <main className="overflow-auto fade grid items-center mt-1 mb-3 before:h-5">
           <AnimatePresence mode="wait">
             {activeEntry ? (
               <motion.div key="timer" {...motionProps}>
