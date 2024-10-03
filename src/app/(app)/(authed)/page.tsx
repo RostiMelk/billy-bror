d5b272876e3ab6fbd17e31a9bea51e52c91fb422
Rostislav Melkumyan <rosti@sanity.io>
2024-10-03 19:00:22 +0200

    Update page.tsx

--- src/app/(app)/(authed)/page.tsx
+++ src/app/(app)/(authed)/page.tsx
@@ -185,7 +185,7 @@ export default function Home() {
           ) : (
             <>
               <p className="text-sm text-muted-foreground font-medium">
-                Er du og Billy klare for en tur?
+                Er du og Billy klare for tur?
               </p>
               <Button
                 size="lg"
