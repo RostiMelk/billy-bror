3f8834a5661a8f0a2e7e5fcc7b672554c68db2db
RostiMelk <hello@rosti.no>
2024-12-22 20:10:03 +0100

    feat: fix height on trip chart

--- src/components/chart-trips-per-day.tsx
+++ src/components/chart-trips-per-day.tsx
@@ -50,7 +50,10 @@ export function ChartTripsPerDay({ chartData }: { chartData: ChartData[] }) {
   const highestValue = Math.max(...chartData.map((data) => data.trips));
 
   return (
-    <ChartContainer config={chartConfig}>
+    <ChartContainer
+      config={chartConfig}
+      className="[&_.recharts-responsive-container]:!h-fit"
+    >
       <div className="relative grid grid-cols-[auto_1fr]">
         <div className="sticky left-0">
           <ResponsiveContainer width={20} height="100%">
